f32672cc10941459512729cfe987b12125faf1df
BigBen3918 <benjamin199551@gmail.com>
2021-10-19 14:56:32 +0300

    fix: server

--- src/components/layouts/wheelset.js
+++ src/components/layouts/wheelset.js
@@ -1,46 +1,40 @@
 import React, { useEffect, useState } from "react";
 
-import wheelImage from "../style/img/wheel.png"
+import wheelImage from "../style/img/wheel.png";
 import rulewheel from "../style/img/rulewheel.png";
 
 import $ from "jquery";
-import { set } from "mongoose";
 
-function Wheel({
-    spinState,
-    currentLength
-}) {
+function Wheel({ spinState, currentLength }) {
+  const [rotateLength, setRotateLength] = useState(0);
+  const [rNum, setRNum] = useState(0);
 
-    const [rotateLength, setRotateLength] = useState(0);
-    const [rNum,setRNum] = useState(0);
+  function getRandom() {
+    var _rNum = rNum + 4;
+    // return Math.floor(rNum + (Math.random() * 30 + 10));
+    // return Math.floor(Math.random() * 5 + 4);
+    setRNum(_rNum);
+    return _rNum;
+  }
 
-    function getRandom() {
-        var _rNum =rNum + 2
-        //  Math.floor(rNum+ Math.random() * 5 + 3);
-        setRNum(_rNum);
-        return _rNum;
+  useEffect(() => {
+    setRotateLength(360 * getRandom() - (360 / 37) * currentLength);
+  }, [currentLength]);
+
+  useEffect(() => {
+    if (spinState === true) {
+      $(".wheel").css("transform", "rotate(" + rotateLength + "deg)");
     }
+  }, [rotateLength]);
 
-    useEffect(() => {
-        console.log("currentLength",currentLength)
-        setRotateLength(360 * getRandom() - (360 / 37) * currentLength);
-        // * getRandom() 
-    }, [currentLength]);
-
-    useEffect(() => {
-        if (spinState === true) {
-            $(".wheel").css("transform", "rotate(" + rotateLength + "deg)");
-        }
-    }, [rotateLength]);
-
-    return (
-        <div className="noselect">
-            <div style={{ position: "relative" }}>
-                <img src={rulewheel} className="rulewheel" alt="NoImg" />
-                <img src={wheelImage} className="wheel" alt="NoImg" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="noselect">
+      <div style={{ position: "relative" }}>
+        <img src={rulewheel} className="rulewheel" alt="NoImg" />
+        <img src={wheelImage} className="wheel" alt="NoImg" />
+      </div>
+    </div>
+  );
 }
 
 export default Wheel;
